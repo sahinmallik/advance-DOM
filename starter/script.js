ed8b231481d6b933d2a79fc3c13ae6e9b1540242
sahinmallik <sahinmallik1999@gmail.com>
2022-01-22 22:14:17 +0530

    Second Commit

--- starter/script.js
+++ starter/script.js
@@ -33,9 +33,63 @@ document.addEventListener('keydown', function (e) {
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
-
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+
+//creating and inserting element
+
+const message = document.createElement('div'); // it is creating a DOM elemnent and store into the variable.
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message); prepend is used to add some dom element as a first child to the html file dynamically or adding DOM element created by JS
+
+header.append(message); //append used to add as a last child.
+// header.append(message.cloneNode(true)); append in multiple position
+// header.before(message);
+// header.after(message);
+
+//delete element
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message);
+  });
+
+//styles
+
+message.style.backgroundColor = '#37383d'; // this style attribute set inline styling and it only can read the inline style only;
+message.style.width = '120%';
+
+// getComputedStyle is used to read all the style added to the element
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//Non-standard
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src); //it will return the absolute url of the img
+console.log(logo.getAttribute('src')); //it will return the relative url of the img
+
+//classes
+
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
