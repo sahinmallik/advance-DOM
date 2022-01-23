922f523d2cacb9f1c8e8af48a3ee2adf53c19754
sahinmallik <sahinmallik1999@gmail.com>
2022-01-23 20:59:44 +0530

    4th Commit

--- starter/script.js
+++ starter/script.js
@@ -30,16 +30,49 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-console.log(document.getElementsByClassName('btn'));
+// Page naviagtaion
+/*
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+*/
+// using event delegation
+/*1. Add event listener to the common parent element
+2. Determine what element originated the event*/
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching Strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//smooth scroling adding
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////EXPERIMENTAL WORK///////////////
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
 
 //creating and inserting element
 /*
@@ -75,60 +108,60 @@ message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 */
 //Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
 
-logo.alt = 'Beautiful minimalist logo';
+// logo.alt = 'Beautiful minimalist logo';
 
 //Non-standard
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
 
-console.log(logo.src); //it will return the absolute url of the img
-console.log(logo.getAttribute('src')); //it will return the relative url of the img
+// console.log(logo.src); //it will return the absolute url of the img
+// console.log(logo.getAttribute('src')); //it will return the relative url of the img
 
 //classes
 
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
 
 //smooth scroling adding
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {
-  const s1cords = section1.getBoundingClientRect();
-  // console.log(s1cords);
-  // console.log(e.target.getBoundingClientRect());
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1cords = section1.getBoundingClientRect();
+// console.log(s1cords);
+// console.log(e.target.getBoundingClientRect());
 
-  // console.log('current scroll (x/y)', window.pageXOffset, window.pageYOffset);
-  // console.log(
-  //   'heigth/width viewport',
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth
-  // );
+// console.log('current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+// console.log(
+//   'heigth/width viewport',
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+// );
 
-  //scroling
-  /* window.scrollTo(
+//scroling
+/* window.scrollTo(
     s1cords.left + window.pageXOffset,
     s1cords.top + window.pageYOffset
   ); // the positing of the function getBoundingClientRect() is relative depending on the view port that why to work scroling method from any position we have to define the absoulute positing to work properly
   */
 
-  // by passing object
+// by passing object
 
-  // window.scrollTo({
-  //   left: s1cords.left + window.pageXOffset,
-  //   top: s1cords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
+// window.scrollTo({
+//   left: s1cords.left + window.pageXOffset,
+//   top: s1cords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
 
-  //Modern Way
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+//Modern Way
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
 
 // old way of listning events is using on before the event and then implementing the functions
 
